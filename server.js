eb1eb4e2c3704caee01e013640dd1acc9f170164
Joel Essienobong <Joddeilove@gmail.com>
2025-08-15 09:53:14 +0100

    Add error handling middleware to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/taskRoutes')
 const connectDB = require('./connectDB/connect');
 const dotenv = require('dotenv');
+const errorHandler = require('./middleware/errorHandler')
 
 dotenv.config()
 connectDB()
@@ -10,6 +11,7 @@ connectDB()
 //middleware
 app.use(express.static('./public'))
 app.use(express.json())
+app.use(errorHandler)
 
 // routes
 app.use('/api/v1/tasks',tasks)
